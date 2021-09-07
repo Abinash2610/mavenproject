baa4abf5f58180df4ca839ec67ec9b7fed6c1bcd
Abinash2610 <abinashvlr2610@gmail.com>
2021-09-07 23:01:04 +0530

    Username

--- src/org/git/FacebookLogin.java
+++ src/org/git/FacebookLogin.java
@@ -1,5 +1,11 @@
 package org.git;
 
 public class FacebookLogin {
-
+	
+	public static void userName() {
+		System.out.println("user Name");
+	}
+	public static void main(String[] args) {
+	 userName();
+}
 }
